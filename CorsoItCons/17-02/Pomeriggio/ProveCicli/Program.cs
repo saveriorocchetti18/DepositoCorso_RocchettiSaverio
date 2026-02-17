4cfa7bf486dcba56fec2f0be9e1223821392854f
saveriorocchetti18 <saveriorocchetti18@gmail.com>
2026-02-17 17:52:55 +0100

    Add files via upload

---
+++ CorsoItCons/17-02/Pomeriggio/ProveCicli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool continua=true;
+        while (continua)
+        {
+            Console.WriteLine("Ciclo in esecuzione");
+            continua=bool.Parse(Console.ReadLine());
+        }
+
+        int continua2=0;
+        while (continua2 < 10)
+        {
+            Console.WriteLine("Ciclo in esecuzione");
+            continua2+=1;
+            Console.WriteLine(continua2);
+        }
+    }
+}
