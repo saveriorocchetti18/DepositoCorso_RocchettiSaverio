da504634f1dad5c216fd5bfa500dda5dea7123fa
saveriorocchetti18 <saveriorocchetti18@gmail.com>
2026-02-17 12:59:41 +0100

    Add files via upload

---
+++ CorsoItCons/17-02/Mattina/ProveDiFlusso/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a=15;
+        int b=10;
+
+        if (a > b)
+        {
+            Console.WriteLine($"{a}>{b}");
+        }
+    }
+}
