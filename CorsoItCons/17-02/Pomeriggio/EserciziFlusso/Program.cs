0368314260e5819b521d25d2f20fb91c9e84f2eb
saveriorocchetti18 <saveriorocchetti18@gmail.com>
2026-02-17 17:06:12 +0100

    Add files via upload

--- CorsoItCons/17-02/Pomeriggio/EserciziFlusso/Program.cs
+++ CorsoItCons/17-02/Pomeriggio/EserciziFlusso/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
         const int PASSWORD=12345;
         //ESERCIZIO 1
         Console.WriteLine("Inserisci numero:");
@@ -113,6 +114,60 @@ class Program
         else
         {
             Console.WriteLine($"La temperatura inserita convertita in Rankine è: {t/3*9}");
+        } */
+
+        //SWITCH
+
+        //ESERCIZIO 1
+        Console.WriteLine("Inserisci un numero da 1 a 7: ");
+        string n=Console.ReadLine();
+        switch (n)
+        {
+            case "1":
+            Console.WriteLine("Lunedi");
+            break;
+            case "2":
+            Console.WriteLine("Martedi");
+            break;
+            case "3":
+            Console.WriteLine("Mercoledi");
+            break;
+            case "4":
+            Console.WriteLine("Giovedi");
+            break;
+            case "5":
+            Console.WriteLine("Venerdi");
+            break;
+            case "6":
+            Console.WriteLine("Sabato");
+            break;
+            default:
+            Console.WriteLine("Domenica");
+            break;
         }
-    }
+
+        //ESERCIZIO 2
+        Console.WriteLine("Quale figura geometrica vuoi considerare? (Quadrato, Cerchio o Triangolo)");
+        string figura=Console.ReadLine();
+
+        switch (figura)
+        {
+            case "Quadrato":
+            Console.WriteLine("Inserisci la misura del lato: ");
+            int lato=int.Parse(Console.ReadLine());
+            Console.WriteLine($"La misura dell'area corrisponde a {lato*lato}");
+            break;
+            case "Cerchio":
+            Console.WriteLine("Inserisci la misura del raggio: ");
+            int r=int.Parse(Console.ReadLine());
+            Console.WriteLine($"La misura dell'area corrisponde a {3.14*r*r}");
+            break;
+            default:
+            Console.WriteLine("Inserisci la misura rispettivamente della base e dell'altezza: ");
+            int b=int.Parse(Console.ReadLine());
+            int h=int.Parse(Console.ReadLine());
+            Console.WriteLine($"La misura dell'area corrisponde a {(b*h)/2}");
+            break;
+        }
+    } 
 }
