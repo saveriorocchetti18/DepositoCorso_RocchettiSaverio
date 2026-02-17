ed074d8f0a4c0348195fa047ca07b7ba315eb10b
saveriorocchetti18 <saveriorocchetti18@gmail.com>
2026-02-17 16:03:56 +0100

    Add files via upload

---
+++ CorsoItCons/17-02/Pomeriggio/EserciziFlusso/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int PASSWORD=12345;
+        //ESERCIZIO 1
+        Console.WriteLine("Inserisci numero:");
+        int j=int.Parse(Console.ReadLine());
+        if (j % 2 == 0)
+        {
+            Console.WriteLine("Il numero inserito è pari!");
+        }
+        else
+        {
+            Console.WriteLine("Il numero inserito è dispari!");
+        }
+
+        //ESERCIZIO 2
+        Console.WriteLine("Inserisci la password: ");
+        int p=int.Parse(Console.ReadLine());
+        if (p == PASSWORD)
+        {
+            Console.WriteLine("ACCESSO CONSENTITO");
+        }
+        else
+        {
+            Console.WriteLine("ACCESSO NEGATO");
+        }
+
+        //ESERCIZIO 3 EXTRA
+        Console.WriteLine("Inserisci due numeri:");
+        double n1=double.Parse(Console.ReadLine());
+        double n2=double.Parse(Console.ReadLine());
+        Console.WriteLine("Inserisci un operatore: (+ o -)");
+        char operatore=char.Parse(Console.ReadLine());
+        if(operatore!='+' && operatore!= '-')
+        {
+            Console.WriteLine("Operatore non valido!");
+        }
+        else
+        {
+            if (operatore == '+')
+            {
+                Console.WriteLine($"Il risultato è: {n1+n2}");
+            }
+            else
+            {
+                Console.WriteLine($"Il risultato è: {n1-n2}");
+            }
+        }
+
+        //IF-ELSE IF
+
+        //ESERCIZIO 1
+        Console.WriteLine("Inserisci un voto da 1 a 10: ");
+        int voto=int.Parse(Console.ReadLine());
+        if (voto >= 9)
+        {
+            Console.WriteLine("Ottimo");
+        }
+        else if (voto >= 7)
+        {
+            Console.WriteLine("Buono");
+        }
+        else if (voto >= 5)
+        {
+            Console.WriteLine("Sufficiente");
+        }
+        else
+        {
+            Console.WriteLine("Insufficiente");
+        }
+
+        //ESERCIZIO 2
+        Console.WriteLine("Inserisci l'altezza: ");
+        float altezza=float.Parse(Console.ReadLine());
+        Console.WriteLine("Inserisci il peso: ");
+        float peso=float.Parse(Console.ReadLine());
+
+        float BMI=peso/(altezza*altezza);
+        if (BMI >= 30)
+        {
+            Console.WriteLine("Obesità");
+        }
+        else if (BMI >= 25)
+        {
+            Console.WriteLine("Sovrappeso");
+        }
+        else if (BMI >= 18.5)
+        {
+            Console.WriteLine("Normopeso");
+        }
+        else
+        {
+            Console.WriteLine("Sottopeso");
+        }
+
+        //ESERICIZIO 3 EXTRA
+        Console.WriteLine("Inserisci una temperatura in Celsius");
+        float t=float.Parse(Console.ReadLine());
+        Console.WriteLine("Inserisci la scala verso cui effetturare la conversione: ");
+        string conversione=Console.ReadLine();
+        if (conversione == "Farhenheit")
+        {
+            Console.WriteLine($"La temperatura inserita convertita in Fahrenheit è: {t/3*5}");
+        }
+        else if (conversione == "Kelvin")
+        {
+            Console.WriteLine($"La temperatura inserita convertita in Kelvin è: {t/3*7}");
+        }
+        else
+        {
+            Console.WriteLine($"La temperatura inserita convertita in Rankine è: {t/3*9}");
+        }
+    }
+}
---
+++ CorsoItCons/17-02/Pomeriggio/ProveFlussoIfElse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a=10,b=10;
+        if (a > b)
+        {
+            Console.WriteLine($"{a} maggiore di {b}");
+        }
+        else
+        {
+            Console.WriteLine($"{a} minore o uguale di {b}");
+        }
+
+        int voto=28;
+        if (voto >= 30)
+        {
+            Console.WriteLine("Eccellente");
+        }
+        else if (voto >= 18)
+        {
+            Console.WriteLine("Sufficiente");
+        }
+
+        int eta=15;
+        if (eta >= 18)
+        {
+            Console.WriteLine("Maggiorenne");
+        }
+        else if (eta >= 13)
+        {
+            Console.WriteLine("Adolescente");
+        }
+        else
+        {
+            Console.WriteLine("Bambino");
+        }
+    }
+}
