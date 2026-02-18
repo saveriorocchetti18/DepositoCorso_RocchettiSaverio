acef912e38fefbe1f6e86672979b791d53f25dda
saveriorocchetti18 <saveriorocchetti18@gmail.com>
2026-02-18 16:21:40 +0100

    Add files via upload

---
+++ CorsoItCons/18-02/Pomeriggio/EserciziArray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string scelta;
+        int l;
+        string[]arrays;
+        int[]arrayi;
+        do
+        {
+            Console.WriteLine("Vuoi inserire un array di stringhe o di interi? S per stringhe, I per interi: ");
+            scelta=Console.ReadLine();
+            Console.WriteLine("Quanto deve essere lungo l'array?");
+            l=int.Parse(Console.ReadLine());
+            if (scelta == "S")
+            {
+                arrays=new string[l];
+                for(int i = 0; i < arrays.Length; i++)
+                {
+                    Console.WriteLine($"Inserisci il {i+1} elemento: ");
+                    arrays[i]=Console.ReadLine();
+                }
+
+                Console.WriteLine("Stampa Array: ");
+                for(int i = 0; i < arrays.Length; i++)
+                {
+                    Console.WriteLine(arrays[i]);
+                }
+            }
+            else
+            {
+                arrayi=new int[l];
+                for(int i = 0; i < arrayi.Length; i++)
+                {
+                    Console.WriteLine($"Inserisci il {i+1} elemento: ");
+                    arrayi[i]=int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Stampa Array: ");
+                for(int i = 0; i < arrayi.Length; i++)
+                {
+                    Console.WriteLine(arrayi[i]);
+                }
+            }
+
+            Console.WriteLine("Vuoi ripetere? Premere Si per ripetere, No per uscire: ");
+            scelta=Console.ReadLine();
+        }
+        while(scelta!="No");
+
+        Console.WriteLine("Sei uscito correttamente dal programma!");
+    }
+}
---
+++ CorsoItCons/18-02/Pomeriggio/ProveArrayCollezioni/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*int[]voti=new int[5];
+        for(int i = 0; i < voti.Length; i++)
+        {
+            Console.WriteLine($"Inserisci il {i+1} voto");
+            voti[i]=int.Parse(Console.ReadLine());
+        }
+
+        //CALCOLO DELLA MEDIA
+        float somma=0;
+        for(int i = 0; i < voti.Length; i++)
+        {
+            somma+=voti[i];
+        }
+
+        float media=somma/voti.Length;
+        Console.WriteLine("La media corrisponde a "+media);
+
+
+        string parola="CSharp";
+        Console.WriteLine("Caratteri della stringa: ");
+        foreach(char carattere in parola)
+        {
+            Console.WriteLine(carattere);
+        }*/
+
+        int[]prova=new int[4];
+        for(int i = 0; i < prova.Length; i++)
+        {
+            Console.WriteLine($"Inserisci il {i+1} numero");
+            prova[i]=int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Numeri dell'array: ");
+        foreach(int cifra in prova)
+        {
+            Console.WriteLine(cifra);
+        }
+    }
+}
