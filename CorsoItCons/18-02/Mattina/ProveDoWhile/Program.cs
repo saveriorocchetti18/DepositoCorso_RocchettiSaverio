225cf1163267feeb1b34a310186eaab79bc45669
saveriorocchetti18 <saveriorocchetti18@gmail.com>
2026-02-18 13:02:49 +0100

    Add files via upload

---
+++ CorsoItCons/18-02/Mattina/ProveDoWhile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool continua=true;
+        do
+        {
+            Console.WriteLine("Ciclo in esecuzione");
+            continua=bool.Parse(Console.ReadLine());
+        }
+        while(!continua);
+    }
+}
