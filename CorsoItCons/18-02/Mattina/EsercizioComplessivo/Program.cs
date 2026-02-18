a698cd17fc8873a7abc785e0d5f48e66fcba4d82
saveriorocchetti18 <saveriorocchetti18@gmail.com>
2026-02-18 14:59:31 +0100

    Add files via upload

--- CorsoItCons/18-02/Mattina/EsercizioComplessivo/Program.cs
+++ CorsoItCons/18-02/Mattina/EsercizioComplessivo/Program.cs
@@ -4,46 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        string inizio,nome_utente,password,utente_acc,password_acc,scelta;
+        string inizio,nomeutente="",password="",utenteacc,passwordacc,scelta;
         int timer;
+        Console.WriteLine("SISTEMA UTENTE\n1.REGISTRAZIONE\n2.ACCESSO\n3.ESCI");
+        inizio=Console.ReadLine();
         do
         {
-            Console.WriteLine("SISTEMA UTENTE\n1.REGISTRAZIONE\n2.ACCESSO");
-            inizio=Console.ReadLine();
             switch (inizio)
             {
-                case 1:
-                    Console.ReadLine("Inserisci un nome utente:");
-                    nome_utente=Console.ReadLine();
-                    Console.ReadLine("Inserisci una password:");
-                    password_utente=Console.ReadLine();
+                case "1":
+                    Console.WriteLine("Inserisci un nome utente:");
+                    nomeutente=Console.ReadLine();
+                    Console.WriteLine("Inserisci una password:");
+                    password=Console.ReadLine();
                     Console.WriteLine("Registrazione avvenuta con successo");
                     break;
-                case 2:
+                case "2":
                     Console.WriteLine("Inserisci il tuo nome utente: ");
-                    utente_acc=Console.ReadLine();
+                    utenteacc=Console.ReadLine();
                     Console.WriteLine("Inserisci la tua password: ");
-                    password_acc=Console.ReadLine();
-                    if(utente_acc!=nome_utente || password_acc != password)
+                    passwordacc=Console.ReadLine();
+                    if(utenteacc!=nomeutente || passwordacc != password)
                     {
                         Console.WriteLine("Nome utente o password errati.");
                     }
                     else
                     {
-                        Console.WriteLine("Accesso eseguito correttamente. Inserire un numero per il conto alla rovescia: ");
-                        timer=int.Parse(Console.ReadLine());
-                        for(int i = timer; i >= 0; i--)
+                        Console.WriteLine("Accesso eseguito correttamente");
+                        do
                         {
-                            Console.WriteLine(i);
+                            Console.WriteLine("Inserire il numero da cui far partire il conto alla rovescia: ");
+                            timer=int.Parse(Console.ReadLine());
+                            for(int i = timer; i >= 0; i--)
+                            {
+                                Console.WriteLine(i);
+                            }
+                            Console.WriteLine("Vuoi ripetere il conto alla rovescia? Premere Si per ripetere, No per uscire: ");
+                            scelta=Console.ReadLine();
                         }
+                        while(scelta=="Si");
                     }
                     break;
                 default:
+                Console.WriteLine("Sei uscito correttamente dal programma!");
                 break;
             }
-            Console.WriteLine("Vuoi uscire dal programma o vuoi ripetere? Premere Si per continuare, No per uscire: ");
-            scelta=Console.ReadLine();
+            if (inizio != "3")
+            {
+                Console.WriteLine("SISTEMA UTENTE\n1.REGISTRAZIONE\n2.ACCESSO\n3.ESCI");
+                inizio=Console.ReadLine();
+                if (inizio == "3")
+                {
+                    Console.WriteLine("Sei uscito correttamente dal programma!");
+                }
+            }
         }
-        while(scelta=="Si"); 
+        while(inizio!="3"); 
     }
 }
